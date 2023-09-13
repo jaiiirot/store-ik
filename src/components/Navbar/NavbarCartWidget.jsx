190e87f8a9ed527a6c0ca87f8435231b4e5e0599
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-09-12 22:29:39 -0300

    update

--- src/components/Navbar/NavbarCartWidget.jsx
+++ src/components/Navbar/NavbarCartWidget.jsx
@@ -1,11 +1,7 @@
-/* import {
-  MagnifyingGlassCircleIcon,
-  ShoppingBagIcon,
-  HeartIcon,
-} from "@heroicons/react/24/solid"; */
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { DataProductosContext } from "../../context/DatosProductos";
+
 function CartWidget() {
   const { card } = useContext(DataProductosContext);
   return (
@@ -14,10 +10,7 @@ function CartWidget() {
       <div className="ml-4 flow-root lg:ml-6">
         <Link to={"card/"}>
           <div className="group -m-2 flex items-center p-2">
-            {/* <ShoppingBagIcon
-              className="h-4 w-4 md:h-6 md:w-6 flex-shrink-0 text-gray-900 hover:text-gray-800 cursor-pointer"
-              aria-hidden="true"
-            /> */}
+            <i class="fa-solid fa-cart-shopping h-4 w-4 md:h-6 md:w-6 flex-shrink-0 text-gray-900 hover:text-gray-800 cursor-pointer"></i>
             <span className="ml-2 text-sm font-medium text-gray-900 hover:text-gray-800 cursor-pointer">
               {card.reduce((a, b) => a + b.quantity, 0)}
             </span>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import ItemCarrucelContainer from "../components/ItemCarrucelContainer/ItemCarrucelContainer";
+// import ItemCarrucelContainer from "../components/ItemCarrucelContainer/ItemCarrucelContainer";
 import ItemSectionContainer from "../components/ItemSection/ItemSectionContainer";
 import ItemListContainer from "../components/ItemListContainer/ItemListContainer";
 export default function Home() {
   return (
     <div className="home">
       <div className="home__carrucel">
-        <ItemCarrucelContainer />
+        <span></span>
       </div>
       <div className="home__main px-4 py-8 sm:px-6 lg:px-8">
         <div className="home__main-suno">
